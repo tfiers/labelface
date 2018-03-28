ccd4924224648e0910aae423433a8369796cd463
Tomas Fiers <tomas.fiers@gmail.com>
2018-03-28 18:54:12 +0200

    Foray into Vue Components

---
+++ VignetteList.vue
@@ -0,0 +1,14 @@
+<h3>Unlabeled events:</h3>
+<img v-for="event in unlabeledEvents"
+     v-bind:src="imageSrc(event)">
+
+<script>
+module.exports = {
+  data: function () {
+    return {
+      title: 'Unlabeled events',
+      
+    }
+  }
+}
+</script>
